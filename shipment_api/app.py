abc1f57c28d5cc8f03d165a3509cde8cf1c172e3
Thiru K <gadam88@example.com>
2025-09-04 07:24:13 +0530

    port update

--- shipment_api/app.py
+++ shipment_api/app.py
@@ -337,4 +337,4 @@ async def calculate_metrics(
 
 if __name__ == "__main__":
     import uvicorn
-    uvicorn.run("app:app", host="0.0.0.0", port=8000, reload=True)
+    uvicorn.run("app:app", host="0.0.0.0", port=8080, reload=True)
